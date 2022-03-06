4653602b4c8831d9f0fc3548367cac57f7cc25e9
Assadi <assadi.halifa@gmail.com>
2022-03-06 20:03:38 +0100

    update client

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const twig = require("twig");
+const cors = require("cors");
 //Routes
 const fileRoutes = require("./routes/files.routes");
 const annuairesRoutes = require("./routes/annuaires.routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //static fields
 app.use(express.static("client/build"));
